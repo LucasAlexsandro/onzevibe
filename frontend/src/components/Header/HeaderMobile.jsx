26a86886ffb66de22fe6a9554513bd28af91b5e1
lucasalexsandro <devlucasalexsandro@gmail.com>
2023-09-05 10:49:15 -0300

    Update menu responsivo

--- frontend/src/components/Header/HeaderMobile.jsx
+++ frontend/src/components/Header/HeaderMobile.jsx
@@ -33,6 +33,44 @@ export const MenuMobileOpen = styled.div`
   height: 100%;
   background-color: #333;
   transition: left 0.3s ease-in-out;
+
+  button {
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin: 1rem;
+    font-size: 3rem;
+    background: none;
+    border: none;
+    color: white;
+  }
+
+  .menu-options {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    nav {
+      a {
+        margin-top: 1rem;
+        color: #fff;
+        font-size: 2.5rem;
+        font-style: normal;
+        font-weight: 600;
+        line-height: normal;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        .number-link {
+          font-size: 1rem;
+        }
+        &:hover {
+          color: var(--color-primary);
+        }
+      }
+    }
+  }
 `;
 
 export default function HeaderMobile() {
@@ -54,7 +92,34 @@ export default function HeaderMobile() {
         <button onClick={closeMenu}>
           <AiOutlineClose />
         </button>
-        <div className="menu-options"></div>
+        <div className="menu-options">
+          <nav>
+            <a href="#">
+              <span className="number-link">01</span>
+              <span>Home</span>
+            </a>
+            <a href="#">
+              <span className="number-link">02</span>
+              <span>BioLink</span>
+            </a>
+            <a href="#">
+              <span className="number-link">03</span>
+              <span>Sobre</span>
+            </a>
+            <a href="#">
+              <span className="number-link">04</span>
+              <span>Serviços</span>
+            </a>
+            <a href="#">
+              <span className="number-link">05</span>
+              <span>Portfólio</span>
+            </a>
+            <a href="#">
+              <span className="number-link">06</span>
+              <span>Contato</span>
+            </a>
+          </nav>
+        </div>
       </MenuMobileOpen>
     </>
   );
