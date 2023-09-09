25695f8c0e9ea960ee1580877f849efa7f0c73b5
lucasalexsandro <devlucasalexsandro@gmail.com>
2023-09-09 12:25:08 -0300

    Create: created section contact

---
+++ frontend/src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ContactContainer = styled.div`
+  width: 100%;
+  height: 50vh;
+  border: 1px solid red;
+`;
+
+
+export default function Contact() {
+  return (
+    <ContactContainer>Contact</ContactContainer>
+  )
+}
