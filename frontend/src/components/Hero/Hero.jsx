de5c3d04e7fac91f87ffe4552f36c88a1a93b2d7
lucasalexsandro <devlucasalexsandro@gmail.com>
2023-09-09 10:57:13 -0300

    Update: Hero section update content

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,22 +1,20 @@
 import Header from "../Header/Header";
 import { HeroContainer } from "./Styles";
-import Logo1 from "../../assets/ICON3.png"
-import Mar from "../../assets/mar.png"
-import HeaderMobile from "../Header/HeaderMobile";
+import { BsArrowDown } from "react-icons/bs"
+import Logo1 from "../../assets/ICON3.png";
+import Mar from "../../assets/mar.png";
+
 
 export default function Hero() {
   return (
     <HeroContainer>
       <Header />
-      {/* <div className="hero-grid">
-        <h2>AGENCIA</h2>
-        <img src={Logo1} />
-        <h2>DIGITAL</h2>
+      <div className="hero-grid">
+        <h1>Desperte seu negócio pro <br/> <span>digital</span></h1>
       </div>
-      <div className="hero-footer">
-        <h3>vibre na mesma frequência do seu cliente</h3>
-        <img src={Mar} />
-      </div> */}
+      <a href="#service-branding">
+        <BsArrowDown/>
+      </a>
     </HeroContainer>
-  )
+  );
 }
